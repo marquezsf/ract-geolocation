8ed5c181741ef8691ab325ca33f886c5586d5f67
Marquez Green <Marquezsf@gmail.com>
2023-08-24 14:20:12 -0700

    completed app

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import './style.css';
+import northernPic from './image/NorthernHemisphere.jpg'
+import southernPic from './image/SouthernHemisphere.jpg'
 
-const HemisphereDisplay = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.error(error)
-    );
-    return (
-    <div>
-        In Northern Hemisphere
-    </div>
-    )
+const hemisphereConfig = {
+Northern: {
+    text:'it is northern hemisphere',
+    picture: northernPic
+},
+Southern: {
+    text:'it is southernPic hemisphere',
+    picture: southernPic
 }
+
+}
+
+
+
+const HemisphereDisplay = ({latitude}) => {
+  const hemisphere = latitude > 0 ? "Northern" : "Southern ";
+    const {text,picture} = hemisphereConfig[hemisphere]
+    return(
+		<div className={hemisphere} >
+			<div className='ui raised text container segment'>
+				<div className='image'>
+					<img src={picture} alt="hemisphere picture"/>
+				</div>
+				<div className='ui teal bottom attached label'>
+					<h1>{ text }</h1>
+				</div>
+			</div>
+		</div>
+	)
+}
+
 
 export default HemisphereDisplay
--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HemisphereDisplay from './HemisphereDisplay';
 
-   class App extends React.Component {
-    constructor(props){
-      super(props)
-      this.state = { latitude: null, errorMessage: ' '}
-      window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-          this.setState({latitude: position.coords.latitude})
-        },
-        (error) => {
-          this.setState({errorMessage: error.message})
+class App extends React.Component {
 
-        }
-    );
+	state = { latitude: null, errorMessage: '' }
 
-    }
-    render() {
-      if(this.state.errorMessage && !this.state. latitude) {
-        return <div> {this.state.errorMessage} </div>
-      }
-        if(!this. state.errorMessage & this.state. latitude) {
-        return <div> {this. state. latitude} </div>
-        }
-        else {
-        return <div> loading... </div>
-         }
-   }
-  }
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({ latitude: position.coords.latitude })
+			},
+			(error) => {
+				this.setState({ errorMessage: error.message })
+			}
+		);
+	}
+
+	render() {
+		if(this.state.errorMessage && !this.state.latitude) {
+			return <div> {this.state.errorMessage} </div>
+		}
+
+		if(!this.state.errorMessage && this.state.latitude) {
+			return <div> <HemisphereDisplay latitude={this.state.latitude} /> </div>
+		}
+
+		else {
+			return <div>Loading...</div>
+		}
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.querySelector('#root')
+)
 
 ReactDOM.render(
     <App />,
